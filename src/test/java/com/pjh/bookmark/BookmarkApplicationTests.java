44a0bd1c3dd99ad2b7a398289f6eb1e297a9c00e
Jung Hyun Park <rootjh937@gmail.com>
2020-04-26 19:21:27 +0900

    초기 설정

---
+++ src/test/java/com/pjh/bookmark/BookmarkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjh.bookmark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
