cf8559c41c6da14e630e66a5df12c40809b7d49b
Jung Hyun Park <rootjh937dev@gmail.com>
2020-05-02 18:41:28 +0900

    cors origins 기능 해제 추가

--- src/main/java/com/pjh/bookmark/config/WebMvcConfig.java
+++ src/main/java/com/pjh/bookmark/config/WebMvcConfig.java
@@ -4,8 +4,8 @@ import com.pjh.bookmark.interceptor.AuthInterceptor;
 import com.pjh.bookmark.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -13,6 +13,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Autowired
     private AuthService authService;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("*")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowCredentials(false)
+                .maxAge(3600);
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry interceptorRegistry){
