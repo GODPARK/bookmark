e31c0f027507dfd474e94fca76989a100b4a3022
Jung Hyun Park <rootjh937dev@gmail.com>
2020-07-10 23:06:59 +0900

    배포 설정 2

--- src/test/java/com/pjh/bookmark/BookmarkApplicationTests.java
+++ src/test/java/com/pjh/bookmark/BookmarkApplicationTests.java
@@ -7,20 +7,20 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BookmarkApplicationTests {
 
-	@Test
-	void contextLoads() {
-		TokenEncoding tokenEncoding = new TokenEncoding();
-		String pass = "park";
-		String passEncrypt = tokenEncoding.encrypt(pass);
-		System.out.println(passEncrypt);
-		System.out.println(tokenEncoding.decrypt(passEncrypt));
+//	@Test
+//	void contextLoads() {
+//		TokenEncoding tokenEncoding = new TokenEncoding();
+//		String pass = "park";
+//		String passEncrypt = tokenEncoding.encrypt(pass);
+//		System.out.println(passEncrypt);
+//		System.out.println(tokenEncoding.decrypt(passEncrypt));
+//
+//		TokenEncoding tokenEncoding1 = new TokenEncoding();
+//		String passEncrypt1 = tokenEncoding1.encrypt(pass);
+//		System.out.println(passEncrypt1);
+//		System.out.println(tokenEncoding1.decrypt(passEncrypt1));
 
-		TokenEncoding tokenEncoding1 = new TokenEncoding();
-		String passEncrypt1 = tokenEncoding1.encrypt(pass);
-		System.out.println(passEncrypt1);
-		System.out.println(tokenEncoding1.decrypt(passEncrypt1));
 
-
-	}
+//	}
 
 }
